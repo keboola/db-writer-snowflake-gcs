59378bd84c5ac505da9ca13b5529920838e5ed0a
Roman Pištěk <roman.pistek@keboola.com>
2024-03-13 11:09:45 +0100

    Drop unnecessary SnowflakeItemConfig (same as ItemConfig)

--- src/Configuration/ValueObject/SnowflakeExportConfig.php
+++ src/Configuration/ValueObject/SnowflakeExportConfig.php
@@ -6,6 +6,7 @@ namespace Keboola\DbWriter\Configuration\ValueObject;
 
 use Keboola\DbWriterConfig\Configuration\ValueObject\DatabaseConfig;
 use Keboola\DbWriterConfig\Configuration\ValueObject\ExportConfig;
+use Keboola\DbWriterConfig\Configuration\ValueObject\ItemConfig;
 use Keboola\DbWriterConfig\Exception\UserException;
 
 readonly class SnowflakeExportConfig extends ExportConfig
@@ -33,7 +34,7 @@ readonly class SnowflakeExportConfig extends ExportConfig
             $config['incremental'] ?? false,
             $config['export'] ?? true,
             !empty($config['primaryKey']) ? $config['primaryKey'] : null,
-            array_map(fn($v) => SnowflakeItemConfig::fromArray($v), $config['items']),
+            array_map(fn($v) => ItemConfig::fromArray($v), $config['items']),
             $tableFilePath,
         );
     }
